7a72a8736eb6fd1c6b1b5a2f9c7e35441907d23c
Bolaji Oyeneye <bioyeneye@gmail.com>
2020-07-07 20:43:44 +0100

    Initial commit

---
+++ src/JulyChallenge/ArrangingCoins/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArrangingCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfFullStairCase = ArrangeCoins(100);
+            Console.WriteLine($"Number of full staircase rows: {numberOfFullStairCase}");
+        }
+        
+        private static int ArrangeCoins(int n) {
+        
+            if(n < 1) return 0;
+        
+            List<int> shape = new List<int>();
+
+            while(shape.Sum() < n){
+                if(shape.Count == 0) {
+                    shape.Add(1);
+                }
+                else{
+                    int currentSum = shape.Sum();
+                    int rem = n - currentSum;
+                    int index = shape.Count;
+                    if(shape[index-1] >= rem){
+                        shape.Add(rem);
+                    } else{
+                        shape.Add(++index);
+                    }
+                }
+            }
+        
+            return shape.Max();
+        }
+    }
+}
